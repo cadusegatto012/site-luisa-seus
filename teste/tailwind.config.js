a027c351d4952346350500be6e8c4c525d4ef45d
cadusegatto012 <cadusegatto012@gmail.com>
2026-02-10 08:58:37 -0300

    Update tailwind.config.js

--- teste/tailwind.config.js
+++ teste/tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./teste/app/**/*.{js,ts,jsx,tsx,mdx}", // Adicione esta linha
+    "./teste/components/**/*.{js,ts,jsx,tsx,mdx}", // E esta também
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
   ],
   theme: {
     extend: {},
