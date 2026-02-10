7d366386e92df3b0989821eb6c7e6499bc4d691c
cadusegatto012 <cadusegatto012@gmail.com>
2026-02-10 17:49:06 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,7 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./teste/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./teste/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
